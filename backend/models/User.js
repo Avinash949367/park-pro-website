0d3976d47652d74ffc983f69aaf21a54df328ad8
Avinash949367 <avinash46479@gmail.com>
2025-07-31 18:28:25 +0530

    create store admin module

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: String,
@@ -22,5 +23,32 @@ const userSchema = new mongoose.Schema({
     type: Date,
   },
 });
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    // For admin users with default password, don't re-hash
+    if (this.password === 'store@Login.1' && this.role === 'admin') {
+      return next();
+    }
+    
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  // For admin default password, do direct comparison
+  if (this.password === 'store@Login.1' && this.role === 'admin') {
+    return candidatePassword === this.password;
+  }
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
